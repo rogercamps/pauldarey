e0f09192587f9547e1a49cef1c47e76d966ace6d
rogercamps <rogercamps@me.com>
2021-04-16 01:53:22 -0700

    media players are responsive

--- scripts/readMore.js
+++ scripts/readMore.js
@@ -12,5 +12,29 @@ function readMore() {
       btnText.innerHTML = "Read less";
       moreText.style.display = "inline";
   }
+  // if (dots.style.display === "none") {
+  //     dots.style.display = "inline";
+  //     btnText.innerHTML = "Read more";
+  //     moreText.style.display = "none";
+  // } else {
+  //     dots.style.display = "none";
+  //     btnText.innerHTML = "Read less";
+  //     moreText.style.display = "inline";
+  // }
 }
 
+
+// const readMoreBtn = document.querySelector('#myBtn');
+// const text = document.querySelector('.text');
+
+// readMoreBtn.addEventListener('click', event => {
+//   text.classList.toggle('showMore');
+//   if (readMoreBtn.innerText === 'Read more') {
+//     readMore.innerText = 'Read less';
+//   } else {
+//     readMore.innerText = 'Read more';
+//   }
+// });
+
+
+ 
